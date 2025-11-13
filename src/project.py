f14e0b45da32e1e326de97cffaf4c209d7b41f29
JLandii <jacob.e.landis@gmail.com>
2025-11-13 12:02:22 -0600

    created basic sprite class, created player

--- src/project.py
+++ src/project.py
@@ -2,7 +2,7 @@ import os
 import random
 
 import turtle
-turtle.fd(0)
+#turtle.fd(0)
 turtle.speed(0) #speed of animation
 turtle.bgcolor("black")
 turtle.ht()
@@ -10,6 +10,17 @@ turtle.setundobuffer(1) # limits the amount of memory the turtle module uses
 turtle.tracer(1) #changes speed of animation
 
 class Sprite(turtle.Turtle):
+    def __init__(self, spriteshape, color, startx, starty):
+        turtle.Turtle.__init__(self, shape = spriteshape)
+        self.speed(0) #speed of animation 
+        self.penup()
+        self.color(color)
+        #self.fd(0)
+        self.goto(startx, starty)
+        self.speed = 1
+
+#Create sprites
+player = Sprite("triangle", "white", 0, 0)
 
 
 
@@ -20,4 +31,4 @@ class Sprite(turtle.Turtle):
 
 
 
-delay = raw_input("Press enter to finish. >")
+delay = input("Press enter to finish. >")
