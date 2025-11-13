81846e7cb7478ff3b31d2394cd2954ad557a65ad
JLandii <jacob.e.landis@gmail.com>
2025-11-13 14:18:25 -0600

    added missile class, added firing the missile, added enemy collision with missile

--- src/project.py
+++ src/project.py
@@ -71,6 +71,35 @@ class Enemy(Sprite):
         self.speed = 6
         self.setheading(random.randint(0,360))
 
+class Missile(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.speed = 20
+        self.status = "ready"
+        self.goto(-1000, 1000) 
+
+    def fire(self):
+        if self.status == "ready":
+            self.goto(player.xcor(), player.ycor())
+            self.setheading(player.heading())
+            self.status = "firing"
+
+    def move(self):
+        
+        if self.status == "ready":
+            self.goto(-1000, 1000) 
+
+        if self.status == "firing":
+            self.fd(self.speed)
+
+        #Border check
+        if self.xcor() < -290 or self.xcor() > 290 or \
+            self.ycor() < -290 or self.ycor() > 290:
+            self.goto(-1000, 1000)
+            self.status = "ready"
+            
+
 
 #keeps game information
 class Game():
@@ -104,26 +133,34 @@ game.draw_border()
 #Create sprites
 player = Player("triangle", "white", 0, 0)
 enemy = Enemy("circle", "red", -100, 0)
+missile = Missile("triangle", "yellow", 0, 0)
 
 #Keyboard bindings
 turtle.onkey(player.turn_left, "Left")
 turtle.onkey(player.turn_right, "Right")
 turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
+turtle.onkey(missile.fire, "space")
 turtle.listen() #asks turtle to watch for key pressed events
 
 #Main game loop
 while True:
     player.move()
     enemy.move()
+    missile.move()
 
-    #Check for collision
+    #Check for collision with the player
     if player.is_collision(enemy):
         x = random.randint(-250, 250)
         y = random.randint(-250, 250)
         enemy.goto(x, y)
 
-
+    #Check for collision between missile and the enemy
+    if missile.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
+        missile.status = "ready"
 
 
 
