b95c6bb4adf4961f8248d60a9a322a57fb2ef281
JLandii <jacob.e.landis@gmail.com>
2025-11-13 11:57:11 -0600

    added basic turtle methods to set up turtle window

--- src/project.py
+++ src/project.py
@@ -1,4 +1,23 @@
 import os
 import random
-import turtle
 
+import turtle
+turtle.fd(0)
+turtle.speed(0) #speed of animation
+turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1) # limits the amount of memory the turtle module uses
+turtle.tracer(1) #changes speed of animation
+
+class Sprite(turtle.Turtle):
+
+
+
+
+
+
+
+
+
+
+delay = raw_input("Press enter to finish. >")
