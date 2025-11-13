f29a780ee77a49c7de1d6d437422649f21154e44
JLandii <jacob.e.landis@gmail.com>
2025-11-13 13:34:39 -0600

    Added enemy class, added enemy, added enemy collision detection

--- src/project.py
+++ src/project.py
@@ -38,6 +38,16 @@ class Sprite(turtle.Turtle):
             self.sety(-290)
             self.rt(60)
 
+
+    def is_collision(self, other):
+        if (self.xcor() >= (other.xcor() - 20)) and \
+        (self.xcor() <= (other.xcor() + 20)) and \
+        (self.ycor() >= (other.ycor() - 20)) and \
+        (self.ycor() <= (other.ycor() + 20)):
+            return True
+        else:
+            return False
+
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
@@ -54,6 +64,12 @@ class Player(Sprite):
 
     def decelerate(self):
         self.speed -= 1
+
+class Enemy(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 6
+        self.setheading(random.randint(0,360))
 
 
 #keeps game information
@@ -87,6 +103,7 @@ game.draw_border()
 
 #Create sprites
 player = Player("triangle", "white", 0, 0)
+enemy = Enemy("circle", "red", -100, 0)
 
 #Keyboard bindings
 turtle.onkey(player.turn_left, "Left")
@@ -98,7 +115,13 @@ turtle.listen() #asks turtle to watch for key pressed events
 #Main game loop
 while True:
     player.move()
+    enemy.move()
 
+    #Check for collision
+    if player.is_collision(enemy):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        enemy.goto(x, y)
 
 
 
